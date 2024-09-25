38ce5a33949d91e383633f3eeda8f1e7d58212c2
frankzhang <frank.zhang.sa.au@gmail.com>
2024-09-25 21:33:26 +1000

    1. fix   /blog layout issue： because  $post->body  has</div>  , <div>{!! Str::limit($post->body, 200, '...') !!}</div> will cause lost </div>, so use strip_tags($post->body) first. layout is ok.

--- resources/views/components/blog/post.blade.php
+++ resources/views/components/blog/post.blade.php
@@ -1,6 +1,7 @@
 <div class="p-2 bg-white shadow">
     <a href="{{ route('blog.show', $post) }}">
         <h2 class="text-xl font-bold">{{ $post->title }}</h2>
-        <div>{!! Str::limit($post->body, 200, '...') !!}</div>
+
+        <div>{{ Str::limit(strip_tags($post->body), 200, '...') }}</div>
     </a>
 </div>
